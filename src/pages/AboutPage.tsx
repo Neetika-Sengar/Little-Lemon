f82880ad3249f3f7b9b30aaf2b0305cc519b65a5
Neetika <sengar.neetika@gmail.com>
2026-04-05 23:08:17 +0530

    feat(contact-about-us): :sparkles: adds contact and about us page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,5 +1,29 @@
+import { Helmet } from 'react-helmet-async';
+import content from '../assets/locales/en.json';
+
 const AboutPage = () => {
-  return <></>;
+  return (
+    <>
+      <Helmet>
+        <title>{content.aboutPageMetaTitle}</title>
+        <meta name="description" content={content.aboutPageMetaDescription} />
+
+        <meta property="og:title" content={content.aboutPageMetaTitle} />
+        <meta
+          property="og:description"
+          content={content.aboutPageMetaDescription}
+        />
+        <meta property="og:image" content="/logo.png" />
+      </Helmet>
+
+      <div>
+        <h1>{content.aboutPageTitle}</h1>
+        <h2>{content.aboutPageSubtitle}</h2>
+        <p>{content.aboutPageParagraph}</p>
+        <p>{content.aboutPageParagraph2}</p>
+      </div>
+    </>
+  );
 };
 
 export default AboutPage;
--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,36 @@
+import { Helmet } from 'react-helmet-async';
+import Button from '../components/Button/Button';
+import content from '../assets/locales/en.json';
+
 const ContactPage = () => {
-  return <></>;
+  return (
+    <>
+      <Helmet>
+        <title>{content.contactPageMetaTitle}</title>
+        <meta name="description" content={content.contactPageMetaDescription} />
+
+        <meta property="og:title" content={content.contactPageMetaTitle} />
+        <meta
+          property="og:description"
+          content={content.contactPageMetaDescription}
+        />
+        <meta property="og:image" content="/logo.png" />
+      </Helmet>
+
+      <div>
+        <h1>{content.contactPageTitle}</h1>
+        <p>{content.contactPageParagraph}</p>
+
+        <Button
+          variant="contained"
+          color="warning"
+          disabled={false}
+          path="tel:+81-1234567890"
+          label={content.callUs}
+        />
+      </div>
+    </>
+  );
 };
 
 export default ContactPage;
